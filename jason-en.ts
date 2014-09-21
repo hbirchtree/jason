076bc9c8e721dfc3c52fb100c2f19f27f0b16d9b
Håvard Bjerke <h0wy36@gmail.com>
2014-09-21 21:07:49 +0200

    Changes to be committed: 	deleted:    TESV.json 	modified:   jason-en.ts 	deleted:    native-common.json 	deleted:    wine-common.json

--- jason-en.ts
+++ jason-en.ts
@@ -4,37 +4,37 @@
 <context>
     <name>JasonGraphical</name>
     <message>
-        <location filename="jasongraphical.cpp" line="15"/>
+        <location filename="jasongraphical.cpp" line="13"/>
         <source>Close</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="jasongraphical.cpp" line="50"/>
+        <location filename="jasongraphical.cpp" line="58"/>
         <source>Jason information</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="jasongraphical.cpp" line="52"/>
+        <location filename="jasongraphical.cpp" line="60"/>
         <source>Jason warning</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="jasongraphical.cpp" line="54"/>
+        <location filename="jasongraphical.cpp" line="62"/>
         <source>Jason error</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="jasongraphical.cpp" line="59"/>
+        <location filename="jasongraphical.cpp" line="68"/>
         <source>Jason - Detached process</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="jasongraphical.cpp" line="60"/>
+        <location filename="jasongraphical.cpp" line="69"/>
         <source> is currently detached. Close this window to notify Jason when it has been closed properly.</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="jasongraphical.cpp" line="61"/>
+        <location filename="jasongraphical.cpp" line="72"/>
         <source>It is closed</source>
         <translation type="unfinished"></translation>
     </message>
@@ -47,42 +47,152 @@
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="jasonparser.cpp" line="37"/>
+        <location filename="jasonparser.cpp" line="38"/>
+        <location filename="jasonparser.cpp" line="47"/>
+        <source>Error occured</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="jasonparser.cpp" line="39"/>
         <source>Apples is stuck in a tree! We need to call the fire department!
 </source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="jasonparser.cpp" line="43"/>
+        <location filename="jasonparser.cpp" line="48"/>
         <source>Shit.
 </source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="jasonparser.cpp" line="822"/>
+        <location filename="jasonparser.cpp" line="54"/>
+        <source>We are generating a .desktop file now. Please wait for possible on-screen prompts.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="jasonparser.cpp" line="56"/>
+        <source>Desktop file was generated successfully.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="jasonparser.cpp" line="58"/>
+        <source>Nothing more to do.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="jasonparser.cpp" line="91"/>
+        <source>Failed to open file</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="jasonparser.cpp" line="100"/>
+        <source>Failed to import file</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="jasonparser.cpp" line="207"/>
+        <source>Gathering fundamental values</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="jasonparser.cpp" line="209"/>
+        <source>Failed to parse fundamental values. Will not proceed.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="jasonparser.cpp" line="213"/>
+        <source>Gathering active options</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="jasonparser.cpp" line="229"/>
+        <source>Activating main system</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="jasonparser.cpp" line="255"/>
+        <location filename="jasonparser.cpp" line="267"/>
+        <source>Resolving variables</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="jasonparser.cpp" line="257"/>
+        <source>Activating subsystems</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="jasonparser.cpp" line="270"/>
+        <source>Processing preruns and postruns</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="jasonparser.cpp" line="533"/>
+        <source>Variable %s is being slightly problematic</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="jasonparser.cpp" line="625"/>
+        <source>No configuration prefix for the system was provided. Will not proceed.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="jasonparser.cpp" line="637"/>
+        <source>No identifier for the system was provided. Will not proceed.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="jasonparser.cpp" line="890"/>
         <source>WARN: There is no main execution value. Just a heads up.
 </source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="jasonparser.cpp" line="834"/>
+        <location filename="jasonparser.cpp" line="901"/>
         <source>ERROR: No primary command line found. Is there a mismatch between the launcher type and .exec value?
 </source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="jasonparser.cpp" line="1144"/>
-        <source>Launching</source>
+        <location filename="jasonparser.cpp" line="1230"/>
+        <source>Executed: </source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="jasonparser.cpp" line="1158"/>
-        <source>Process exited successfully.</source>
+        <location filename="jasonparser.cpp" line="1231"/>
+        <source>Process returned value </source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="jasonparser.cpp" line="1160"/>
-        <source>Process exited with status</source>
+        <location filename="jasonparser.cpp" line="1232"/>
+        <source>QProcess returned value </source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="jasonparser.cpp" line="1246"/>
+        <source>Process exited with status:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="jasonparser.cpp" line="1248"/>
+        <location filename="jasonparser.cpp" line="1252"/>
+        <source>QProcess exited with status:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="jasonparser.cpp" line="1268"/>
+        <source>Warning: The filename specified for the output .desktop file does not have the correct extension.
+</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="jasonparser.cpp" line="1272"/>
+        <source>Warning: The file exists. Will not proceed.
+</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="jasonparser.cpp" line="1278"/>
+        <source>Warning: Failed to open the output file for writing. Will not proceed.</source>
         <translation type="unfinished"></translation>
     </message>
 </context>
